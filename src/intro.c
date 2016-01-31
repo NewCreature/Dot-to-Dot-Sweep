4abca59b4be2bcf59443ca55bd337f97b3270474
Todd Cope <xander4jesus@yahoo.com>
2016-01-31 10:23:31 -0600

    Adjust intro screen to new display dimensions.

--- src/intro.c
+++ src/intro.c
@@ -29,12 +29,12 @@ void dot_intro_render(void * data)
 
 	al_clear_to_color(al_map_rgb_f(0.5, 0.5, 0.5));
 	al_hold_bitmap_drawing(true);
-	for(i = 0; i < 480 / 16; i++)
+	for(i = 0; i < 540 / 16; i++)
 	{
-		al_draw_bitmap(app->bitmap[DOT_BITMAP_BALL_RED + i % 6], i * 16, sin((float)(i * 2 + app->tick) / 10.0) * 32 + 64, 0);
-		al_draw_bitmap(app->bitmap[DOT_BITMAP_BALL_RED + i % 6], i * 16, cos((float)(i * 2 + app->tick) / 10.0) * 32 + 480 - 64 - 16 - 1, 0);
+		al_draw_bitmap(app->bitmap[DOT_BITMAP_BALL_RED + i % 6], i * 16 + 6, sin((float)(i * 2 + app->tick) / 10.0) * 32 + 64, 0);
+		al_draw_bitmap(app->bitmap[DOT_BITMAP_BALL_RED + i % 6], i * 16 + 6, cos((float)(i * 2 + app->tick) / 10.0) * 32 + DOT_GAME_PLAYFIELD_HEIGHT - 64 - 16 - 1, 0);
 	}
 	dot_game_render_hud(data);
-	dot_shadow_text(app->font, t3f_color_white, al_map_rgba_f(0.0, 0.0, 0.0, 0.5), 240, 240 - 8, 2, 2, ALLEGRO_ALIGN_CENTRE, "Click anywhere to begin...");
+	dot_shadow_text(app->font, t3f_color_white, al_map_rgba_f(0.0, 0.0, 0.0, 0.5), DOT_GAME_PLAYFIELD_WIDTH / 2, DOT_GAME_PLAYFIELD_HEIGHT / 2 - 8, 2, 2, ALLEGRO_ALIGN_CENTRE, "Click anywhere to begin...");
 	al_hold_bitmap_drawing(false);
 }
