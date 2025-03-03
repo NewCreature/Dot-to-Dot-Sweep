018acab104b989310a7e2987f47d75a6ffabc9a3
Todd Cope <todd@tcubedsoftware.com>
2025-03-03 17:01:25 -0600

    Add 'dot_enable_soft_cursor()' and 'dot_render_cursor()' for enabling and rendering a software cursor.

--- src/mouse.c
+++ src/mouse.c
@@ -2,6 +2,27 @@
 
 static bool _dot_mouse_enabled = true;
 static bool _dot_mouse_showing = true;
+static bool _dot_mouse_soft = false;
+
+void dot_enable_soft_cursor(bool onoff)
+{
+  if(onoff)
+  {
+    _dot_mouse_soft = true;
+    if(_dot_mouse_showing)
+    {
+      al_hide_mouse_cursor(t3f_display);
+    }
+  }
+  else
+  {
+    _dot_mouse_soft = false;
+    if(_dot_mouse_showing)
+    {
+      al_show_mouse_cursor(t3f_display);
+    }
+  }
+}
 
 void dot_enable_mouse_cursor(bool onoff)
 {
@@ -14,7 +35,10 @@ void dot_mouse_cursor_logic(void)
   {
     if(!_dot_mouse_showing)
     {
-      al_show_mouse_cursor(t3f_display);
+      if(!_dot_mouse_soft)
+      {
+        al_show_mouse_cursor(t3f_display);
+      }
       _dot_mouse_showing = true;
     }
   }
@@ -22,8 +46,19 @@ void dot_mouse_cursor_logic(void)
   {
     if(_dot_mouse_showing)
     {
-      al_hide_mouse_cursor(t3f_display);
+      if(!_dot_mouse_soft)
+      {
+        al_hide_mouse_cursor(t3f_display);
+      }
       _dot_mouse_showing = false;
     }
+  }
+}
+
+void dot_render_cursor(T3F_BITMAP * bp)
+{
+  if(_dot_mouse_soft && _dot_mouse_enabled)
+  {
+    t3f_draw_bitmap(bp, t3f_color_white, t3f_get_mouse_x(), t3f_get_mouse_y(), 0.0, 0);
   }
 }
--- src/mouse.h
+++ src/mouse.h
@@ -1,7 +1,9 @@
 #ifndef DOT_MOUSE_H
 #define DOT_MOUSE_H
 
+void dot_enable_soft_cursor(bool onoff);
 void dot_enable_mouse_cursor(bool onoff);
 void dot_mouse_cursor_logic(void);
+void dot_render_cursor(T3F_BITMAP * bp);
 
 #endif
